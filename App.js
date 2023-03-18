f27c441e451a2093d015ca0917e1260505938fbd
Divya <divyagarg4747@gmail.com>
2023-03-19 01:15:07 +0530

    lol

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import SearchBar from './components/SearchBar';
+import image from './assets/omelette.png';
+import LocationCard from './components/LocationCard';
+import FoodCard2 from './components/FoodCard2';
 
 export default function App() {
   return (
+    // <View style={styles.container}>
+    //   <Text>lol</Text>
+    //   <StatusBar style="auto" />
+    // </View>
     <View style={styles.container}>
-      <Text>lol</Text>
-      <StatusBar style="auto" />
+    {/* <SearchBar  textInput="search here"/> */}
+    {/* <FoodCard2 height="152" width="156" heading1='Food'  image={image}/> */}
+    <LocationCard text='Brahma canteen'/>
+    {/* <FoodCard height="152px" width="156px" heading1='Food' heading2='get food from all over campus'  image={image}/> */}
     </View>
   );
 }
@@ -13,7 +23,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
